3dfa76ba433823f1aff7f25c37d6eed66c1ba463
Alex Rowe <alexrowe@Alexs-MBP.fios-router.home>
2022-06-11 02:47:30 -0400

    Almost completed Gradient Descent function

--- linearRegression.py
+++ linearRegression.py
@@ -7,32 +7,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 
+def abline(slope, intercept):
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--')
+
 def get_gradient_at_b(x, y, b, m):
-  N = len(x)
+  N = len(upperConfidenceInterval)
   diff = 0
   for i in range(N):
-    x_val = x[i]
-    y_val = y[i]
-    diff += (y_val - ((m * x_val) + b))
+    x_val = upperConfidenceInterval[i]
+    y_val = averageDeaths[i]
+    diff += (y_val - ((m[i] * x_val) + b[i]))
   b_gradient = -(2/N) * diff  
   return b_gradient
 
 def get_gradient_at_m(x, y, b, m):
-  N = len(x)
+  N = len(upperConfidenceInterval)
   diff = 0
   for i in range(N):
-      x_val = x[i]
-      y_val = y[i]
+      x_val = upperConfidenceInterval[i]
+      y_val = averageDeaths[i]
       diff += x_val * (y_val - ((m * x_val) + b))
   m_gradient = -(2/N) * diff  
   return m_gradient
 
-def step_gradient(x, y, b_current, m_current):
+def step_gradient(x, y, b_current, m_current, learning_rate):
   b_gradient = get_gradient_at_b(x, y, b_current, m_current)
   m_gradient = get_gradient_at_m(x, y, b_current, m_current)
-  b = b_current - (0.01 * b_gradient)
-  m = m_current - (0.01 * m_gradient)
+  b = b_current - (learning_rate * b_gradient)
+  m = m_current - (learning_rate * m_gradient)
   return (b, m)
+
+def gradient_descent(x, y, learning_rate, num_iterations):
+  b = 0
+  m = 0
+  for i in range(num_iterations):
+    b,m = step_gradient(b, m, x, y, learning_rate)
+  return [b, m]
 
 # current intercept guess:
 b = 0
@@ -40,10 +53,11 @@ b = 0
 m = 0
 
 # Call your function here to update b and m
-b, m = step_gradient(upperConfidenceInterval, averageDeaths, b, m)
+b, m = gradient_descent(upperConfidenceInterval, averageDeaths, 0.01, 100)
 print(b, m)
 
 plt.plot(upperConfidenceInterval, averageDeaths, "o")
 plt.xlabel("Upper Confidence Interval")
 plt.ylabel("Average Deaths per Year")
+abline(b, m)
 plt.show()
