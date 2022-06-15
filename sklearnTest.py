30117be0ab063afee516e5e40b6080a11e9058a1
Alex Rowe <alexrowe@Alexs-MBP.fios-router.home>
2022-06-15 01:48:49 -0400

    Linear Regression achieved

--- sklearnTest.py
+++ sklearnTest.py
@@ -1,5 +1,5 @@
-from sklearn.linear_model import LinearRegression as lr
-import sklearn
+from sklearn.linear_model import LinearRegression
+import sklearn.linear_model
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,18 +9,17 @@ x = [144, 155, 165, 141, 134, 138, 138, 144, 130, 121, 124, 112, 119, 122, 120, 
 y = [43, 18, 5, 8, 19, 22, 16, 14, 9, 17, 33, 25, 21, 17, 17, 22, 6, 29, 39]
 xRs = x.copy()
 yRs = y.copy()
-y_predicted = []
 
-xRs = np.reshape(yRs, (-1, 1))
-dataSet = lr()
-model = dataSet.fit(xRs, yRs)
-for i in range(len(x)):
-    y_predicted.append(dataSet.predict(xRs))
+xRs = np.reshape(xRs, (-1, 1))
+yRs = np.reshape(yRs, (-1, 1))
+line_fitter = LinearRegression()
+line_fitter.fit(xRs, yRs)
+best_fit = line_fitter.predict(xRs)
 
+print (line_fitter.intercept_)
 
 plt.plot(x, y, "o")
+plt.plot(x, best_fit, "--")
 plt.xlabel("Upper Confidence Interval")
 plt.ylabel("Average Deaths")
 plt.show()
-
-print (y_predicted)
