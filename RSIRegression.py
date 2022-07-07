2c3c213962209c874b1cbce969032b9197e9fa58
Alex Rowe <alexrowe@Alexs-MBP.fios-router.home>
2022-07-07 00:09:04 -0400

    Integrated Taapi API into RSI Regression program

--- RSIRegression.py
+++ RSIRegression.py
@@ -2,29 +2,38 @@ from sklearn.linear_model import LinearRegression
 from matplotlib.animation import FuncAnimation
 from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 import requests
-import time
 import json
 
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-parameters = {
+cParameters = {
     'fsyms': 'ETH',
     'tsyms': 'USD',
     'api_key': '1ab438d71f07a76e335c7638ac5c6f3c2d7505ef0bb153496d14819454a7ebc0'
 }
 
+tParameters = {
+    'exchange': 'binance',
+    'interval': '1m',
+    'symbol': 'BTC/USDT',
+    'secret': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjJjNTEwNjA2YzI4YjU1Y2Q1NmEyMTJiIiwiaWF0IjoxNjU3MDgxOTUyLCJleHAiOjMzMTYxNTQ1OTUyfQ.Jy2RKBIaxY02zRq4MtNeu1Q2-PY8gjmGpUcR6KYK79o'
+}
+
 prices = []
+rsis = []
 now = []
 
 def findPrice(i):
-    # Get BTC price from Crypto Compare API, format as integer priceVal in USD
-    getPrice = requests.get("https://min-api.cryptocompare.com/data/pricemulti?", params=parameters)
+    # Get BTC price from Crypto Compare API, format as int priceVal in USD
+    getPrice = requests.get("https://min-api.cryptocompare.com/data/pricemulti?", params=cParameters)
     data = getPrice.json()
     priceUSD = data['ETH']
     priceVal = priceUSD['USD']
+
+    # Get BTC RSI from Taapi API, format as int rsi
+    getRSI = requests.get("https://api.taapi.io/rsi?", params = tParameters)
+    rsiData = getRSI.json()
+    rsi = rsiData['value']
 
     # Get current time, format as HH:MM:SS
     currentTime = datetime.now()
@@ -34,34 +43,29 @@ def findPrice(i):
     print("Price at " + str(currentTime.strftime("%H:%M:%S")) + ": " + str(priceVal))
     prices.append(priceVal)
 
-    # Finds RSI
-    if (len(prices) >= 14):
-        totalGain = 0
-        totalLoss = 0
-        index = len(prices) - 13 + i
-        for i in range(14):
-            if (i > 1 and i <= 14):
-                if (prices[index] >= prices[index - 1]):
-                    totalGain += (prices[index] - prices[index - 1])
-                elif (prices[index] <= prices[index - 1]):
-                    totalLoss += (prices[index - 1] - prices[index])
-        avgGain = totalGain / 14
-        avgLoss = totalLoss / 14
-        RS = avgGain / avgLoss
-        RSI = 100 / (1 + RS)
-        print("RSI: " + str(RSI))
-        # print(totalGain)
-        # print(totalLoss)
-        # print(avgGain)
-        # print(avgLoss)
+    # Print time retrieved and RSI, and append to list rsis
+    print("RSI at " + str(currentTime.strftime("%H:%M:%S")) + ": " + str(rsi))
+    rsis.append(rsi)
 
     # Live plots prices
+    plt.figure(0)
     plt.cla()
-    plt.xlabel("Time (HH:MM:SS")
+    plt.title("Prices vs. Time")
+    plt.xlabel("Time (HH:MM:SS)")
     plt.ylabel("Price (USD)")
     plt.plot(now, prices, "-")
 
+    plt.figure(1)
+    plt.cla()
+    plt.title("RSI vs. Time")
+    plt.xlabel("Time (HH:MM:SS)")
+    plt.ylabel("RSI")
+    plt.plot(now, rsis, "o")
+
 # Live plots prices using FuncAnimation from matplotlib.animation
-ani = FuncAnimation(plt.gcf(), findPrice, interval=1000)
+ani = FuncAnimation(plt.gcf(), findPrice, interval=60000)
+plt.figure(0)
+plt.show()
+plt.figure(1)
 plt.show()
 
--- taapi.py
+++ taapi.py
@@ -4,5 +4,14 @@ import numpy as np
 import requests
 import matplotlib.pyplot as plt
 
-test = requests.get("https://api.taapi.io/rsi?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjJjNTEwNjA2YzI4YjU1Y2Q1NmEyMTJiIiwiaWF0IjoxNjU3MDgxOTUyLCJleHAiOjMzMTYxNTQ1OTUyfQ.Jy2RKBIaxY02zRq4MtNeu1Q2-PY8gjmGpUcR6KYK79o&exchange=binance&symbol=BTC/USDT&interval=1h")
-print(test.json())
+parameters = {
+    'exchange': 'binance',
+    'interval': '1h',
+    'symbol': 'BTC/USDT',
+    'secret': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjJjNTEwNjA2YzI4YjU1Y2Q1NmEyMTJiIiwiaWF0IjoxNjU3MDgxOTUyLCJleHAiOjMzMTYxNTQ1OTUyfQ.Jy2RKBIaxY02zRq4MtNeu1Q2-PY8gjmGpUcR6KYK79o'
+}
+
+getData = requests.get("https://api.taapi.io/rsi?", params = parameters)
+dataJSON = getData.json()
+rsi = dataJSON['value']
+print(rsi)
